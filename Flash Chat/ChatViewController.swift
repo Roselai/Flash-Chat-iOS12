a45397af9c73921af07cccd8dbca7b6ef6c37c3c
Tabassum Shaikh <shukti.azad@gmail.com>
2018-10-09 10:04:14 -0400

    added tableViewDelegate methods and custom cell

--- Flash Chat/ChatViewController.swift
+++ Flash Chat/ChatViewController.swift
@@ -10,12 +10,14 @@ import UIKit
 import Firebase
 
 
-class ChatViewController: UIViewController {
+class ChatViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
-    // Declare instance variables here
+    
+    
+    //MARK: VARIABLES
 
     
-    // We've pre-linked the IBOutlets
+    //MARK: IBOUTLETS
     @IBOutlet var heightConstraint: NSLayoutConstraint!
     @IBOutlet var sendButton: UIButton!
     @IBOutlet var messageTextfield: UITextField!
@@ -26,9 +28,8 @@ class ChatViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        //TODO: Set yourself as the delegate and datasource here:
-        
-        
+        messageTableView.delegate = self
+        messageTableView.dataSource = self
         
         //TODO: Set yourself as the delegate of the text field here:
 
@@ -37,9 +38,10 @@ class ChatViewController: UIViewController {
         //TODO: Set the tapGesture here:
         
         
-
-        //TODO: Register your MessageCell.xib file here:
-
+        
+        messageTableView.register(UINib(nibName: "MessageCell", bundle: nil), forCellReuseIdentifier: "customMessageCell")
+        
+        configureTableView()
         
     }
 
@@ -47,14 +49,18 @@ class ChatViewController: UIViewController {
     
     //MARK: - TableView DataSource Methods
     
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
     
-    
-    //TODO: Declare cellForRowAtIndexPath here:
-    
-    
-    
-    //TODO: Declare numberOfRowsInSection here:
-    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "customMessageCell", for: indexPath) as! CustomMessageCell
+        
+        let messageArray = ["First Message", "Second Message", "Third Message"]
+        cell.messageBody.text = messageArray[indexPath.row]
+        
+        return cell
+    }
     
     
     //TODO: Declare tableViewTapped here:
@@ -62,7 +68,10 @@ class ChatViewController: UIViewController {
     
     
     //TODO: Declare configureTableView here:
-    
+    func configureTableView(){
+        messageTableView.rowHeight = UITableView.automaticDimension
+        messageTableView.estimatedRowHeight = 120.0
+    }
     
     
     ///////////////////////////////////////////
@@ -107,7 +116,6 @@ class ChatViewController: UIViewController {
     
     @IBAction func logOutPressed(_ sender: AnyObject) {
         
-        //TODO: Log out the user and send them back to WelcomeViewController
         do {
             try Auth.auth().signOut()
         } catch {
