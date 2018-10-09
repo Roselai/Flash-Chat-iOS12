2840aac530f25c4aa7d8cafb4af395129311b8ce
Tabassum Shaikh <shukti.azad@gmail.com>
2018-10-09 11:22:29 -0400

    successfully sending and receiving messages from firebase

--- Flash Chat/ChatViewController.swift
+++ Flash Chat/ChatViewController.swift
@@ -12,10 +12,8 @@ import Firebase
 
 class ChatViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UITextFieldDelegate {
     
-    
-    
     //MARK: VARIABLES
-
+    var messageArray: [Message] = [Message]()
     
     //MARK: IBOUTLETS
     @IBOutlet var heightConstraint: NSLayoutConstraint!
@@ -23,8 +21,7 @@ class ChatViewController: UIViewController, UITableViewDelegate, UITableViewData
     @IBOutlet var messageTextfield: UITextField!
     @IBOutlet var messageTableView: UITableView!
     
-    
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -40,6 +37,7 @@ class ChatViewController: UIViewController, UITableViewDelegate, UITableViewData
         
         configureTableView()
         
+        retreiveMessages()
     }
 
     ///////////////////////////////////////////
@@ -47,14 +45,16 @@ class ChatViewController: UIViewController, UITableViewDelegate, UITableViewData
     //MARK: - TableView DataSource Methods
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3
+        return messageArray.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "customMessageCell", for: indexPath) as! CustomMessageCell
         
-        let messageArray = ["First Message", "Second Message", "Third Message"]
-        cell.messageBody.text = messageArray[indexPath.row]
+        
+        cell.messageBody.text = messageArray[indexPath.row].messageBody
+        cell.senderUsername.text = messageArray[indexPath.row].sender
+        cell.avatarImageView.image = UIImage(named: "egg")
         
         return cell
     }
@@ -97,10 +97,6 @@ class ChatViewController: UIViewController, UITableViewDelegate, UITableViewData
     
     //MARK: - Send & Recieve from Firebase
     
-    
-    
-    
-    
     @IBAction func sendPressed(_ sender: AnyObject) {
         
         messageTextfield.endEditing(true)
@@ -129,8 +125,28 @@ class ChatViewController: UIViewController, UITableViewDelegate, UITableViewData
         }
     }
     
-    //TODO: Create the retrieveMessages method here:
-    
+
+    func retreiveMessages() {
+        
+        let messageDB = Database.database().reference().child("Messages")
+        
+        messageDB.observe(.childAdded) { (snapshot) in
+            
+            let snapshotValue = snapshot.value as! Dictionary<String, String>
+            let text = snapshotValue["MessageBody"]!
+            let sender = snapshotValue["Sender"]!
+            
+            let message = Message()
+            message.messageBody = text
+            message.sender = sender
+            
+            self.messageArray.append(message)
+            
+            self.configureTableView()
+            self.messageTableView.reloadData()
+        }
+        
+    }
     
 
     
